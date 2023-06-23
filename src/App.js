a2794fbff459ee73f0784ff12816f1b8ebfa6bd0
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2023-06-23 20:29:08 +0530

    Added routing necessary for deploying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Home from './components/Home/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, createBrowserHistory } from 'react-router-dom';
 import ProjectDetails from './components/Work/projects/ProjectDetails';
 function App() {
+  const history = createBrowserHistory({
+    basename: process.env.PUBLIC_URL,
+  });
   return (
     <div className="App">
-
+<Home></Home>
       
-      <Router>
+      <Router history={history}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects/:id" component={ProjectDetails} />
-        {/* <Route path="/" component={ProjectDetails} /> */}
-      
+       
       </Switch>
     </Router>
     </div>
