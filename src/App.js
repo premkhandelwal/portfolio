5977e0a712524c6b7a29a6b5f29d41b2c15082b3
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2023-06-23 21:10:25 +0530

    Added basename

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Home from './components/Home/Home';
-import {BrowserRouter as Router, createBrowserHistory } from 'history';
-import { Switch, Route, } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import ProjectDetails from './components/Work/projects/ProjectDetails';
+require("dotenv").config();
 function App() {
   const history = createBrowserHistory({
-    basename: process.env.PUBLIC_URL,
+    basename: process.env.REACT_APP_PUBLIC_URL,
   });
   return (
     <div className="App">
-      <Router history={history}>
+      <Router history={history.basename}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/projects/:id" component={ProjectDetails} />
