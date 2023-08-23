ef7a57097b9baa83fd5a1a1a5aa185e62dbba99f
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2023-08-23 16:34:47 +0530

    Updated about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,7 +14,7 @@ const About = () => {
             <div style={{ width: "10%;", margin: "0 auto;" }}>
 
                 <p>
-                As a passionate software developer, I am committed to delivering exceptional value to clients. With a strong academic background in computer science and a genuine love for coding, I possess a diverse range of expertise. My impressive portfolio showcases the successful development of over 14 applications, including two published projects. I specialize in working with REST APIs and have experience handling both relational and unstructured data. You can expect top-quality solutions and a dedicated approach to their projects from me.
+                As a passionate software developer, I am committed to delivering exceptional value to clients. With a strong academic background in computer science and a genuine love for coding, I possess a diverse range of expertise. My impressive portfolio showcases the successful development of over 18 applications, including four published projects. I specialize in working with REST APIs and have experience handling both relational and unstructured data. You can expect top-quality solutions and a dedicated approach to their projects from me.
                 </p>
             </div>
         </div>
