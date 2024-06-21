68ef0be8e42861e92a52fd7bc07c6ef0d40919fe
premkhandelwal <prem.dkhandelwal@gmail.com>
2024-06-21 06:14:13 +0530

    Responsive tech stack

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -13,7 +13,7 @@ export default function TechStack() {
             <div className = "languagesTitle">Frameworks/Languages</div>
             <div className="card-container">
 
-                <TechCard list = {["Flutter","ReactJS", "AngularJS"]} logo = {flutterLogo} variant = "Frontend" text = "Developed more than 14 applications till date. Two of them on Play Store"/>
+                <TechCard list = {["Flutter","ReactJS", "AngularJS"]} logo = {flutterLogo} variant = "Frontend" text = "Developed more than 20 applications till date. Two of them on Play Store"/>
                 <TechCard list = {["NodeJS","C#", "Firebase"]} logo = {nodeLogo} variant = "Backend" text = "Developed backend of various applications using wide support of the library"/>
                 <TechCard list = {["MySQL/MSSQL", "Firestore", "MongoDB", "Postgres"]} logo = {sqlLogo} variant = "SQL" text = "Worked with various kinds of database, integrated them with applications"/>
                 <TechCard list = {["Ethereum","Goerli","Polygon"]} logo = {blockChainLogo} variant = "Blockchain" text = "Developed decentralized application and deployed on various test networks"/>
